b0fe0b50b610533c28e84ab2c97a36cd5cee06fd
Jimmy <jaymeet5087d@gmail.com>
2023-11-25 17:30:37 -0600

    try 4

--- hw5.py
+++ hw5.py
@@ -31,6 +31,7 @@ def send(sock: socket.socket, data: bytes):
     chunk_size = homework5.MAX_PACKET - 4
     seq_number = 0
     window = []
+    window_size = 2  # Maximum packets on the wire
     estimated_rtt = 1.0
     dev_rtt = 0.5
     alpha = 0.125
@@ -40,11 +41,11 @@ def send(sock: socket.socket, data: bytes):
     chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
 
     while seq_number < len(chunks) or window:
-        while seq_number < len(chunks) and len(window) < 2:
+        while seq_number < len(chunks) and len(window) < window_size:
             packet = struct.pack("!I", seq_number) + chunks[seq_number]
             sock.send(packet)
-            window.append((packet, time.time()))
-            logger.info("Sent packet with sequence number %d", seq_number - 1)
+            window.append((seq_number, packet, time.time()))
+            logger.info(f"Sent packet {seq_number}")
             seq_number += 1
 
         sock.settimeout(timeout)
@@ -52,27 +53,27 @@ def send(sock: socket.socket, data: bytes):
             ack, _ = sock.recvfrom(1024)
             ack_number = struct.unpack("!I", ack)[0]
 
-            while window and window[0][0][:4] <= struct.pack("!I", ack_number):
-                sent_time = window.pop(0)[1]
+            # Update window and RTT calculations
+            window = [pkt for pkt in window if pkt[0] > ack_number]
+            if window:
+                sent_time = window[0][2]
                 sample_rtt = time.time() - sent_time
                 estimated_rtt = (1 - alpha) * estimated_rtt + alpha * sample_rtt
                 dev_rtt = (1 - beta) * dev_rtt + beta * abs(sample_rtt - estimated_rtt)
                 timeout = estimated_rtt + 4 * dev_rtt
-
         except socket.timeout:
-            logger.info("Timeout, retransmitting packets in the window")
-            for packet, _ in window:
+            # Resend all packets in the window
+            for _, packet, _ in window:
                 sock.send(packet)
-                logger.info("Retransmitted packet with sequence number %d", _)
+                logger.info("Timeout, retransmitted packet")
 
     # Connection Termination
     fin_packet = struct.pack("!I", 0xFFFFFFFF)
     sock.send(fin_packet)
-
+    logger.info("Sent FIN packet")
     while True:
         try:
             sock.settimeout(2 * timeout)  # Double the timeout for FIN packet
-            logger.info("Fin")
             ack, _ = sock.recvfrom(1024)
             if ack == fin_packet:
                 logger.info("Received FIN-ACK, closing connection")
@@ -80,7 +81,6 @@ def send(sock: socket.socket, data: bytes):
         except socket.timeout:
             sock.send(fin_packet)
             logger.info("Timeout, resending FIN packet")
-
 
 def recv(sock: socket.socket, dest: io.BufferedIOBase) -> int:
     """
@@ -99,25 +99,28 @@ def recv(sock: socket.socket, dest: io.BufferedIOBase) -> int:
     received_data = {}
 
     while True:
-        packet = sock.recv(homework5.MAX_PACKET + 4)
-        if not packet:
+        try:
+            packet = sock.recv(homework5.MAX_PACKET + 4)
+            if not packet:
+                continue
+
+            seq_number, = struct.unpack("!I", packet[:4])
+            if seq_number == 0xFFFFFFFF:
+                logger.info("Received FIN packet, sending FIN-ACK")
+                fin_ack_packet = struct.pack("!I", 0xFFFFFFFF)
+                sock.send(fin_ack_packet)
+                break
+
+            if seq_number >= expected_seq:
+                received_data[seq_number] = packet[4:]
+                while expected_seq in received_data:
+                    dest.write(received_data.pop(expected_seq))
+                    logger.info(f"Received and wrote packet {expected_seq}")
+                    expected_seq += 1
+
+            ack_packet = struct.pack("!I", expected_seq - 1)
+            sock.send(ack_packet)
+        except socket.timeout:
             continue
-
-        seq_number, = struct.unpack("!I", packet[:4])
-        if seq_number == 0xFFFFFFFF:
-            logger.info("Received FIN packet, sending FIN-ACK")
-            fin_ack_packet = struct.pack("!I", 0xFFFFFFFF)
-            sock.send(fin_ack_packet)
-            break
-
-        if seq_number >= expected_seq:
-            received_data[seq_number] = packet[4:]
-            while expected_seq in received_data:
-                dest.write(received_data.pop(expected_seq))
-                logger.info("Received and wrote %d bytes from packet with sequence number %d", len(packet) - 4, seq_number)
-                expected_seq += 1
-
-        ack_packet = struct.pack("!I", expected_seq - 1)
-        sock.send(ack_packet)
 
     return dest.tell()
